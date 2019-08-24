bd5e2b28a4aa62b1b87a0c0f4645488b3d14136b
sequoiap <sequoiap4@gmail.com>
2019-08-24 02:16:52 -0600

    Configurations file for global vars

--- autogator/configurations.py
+++ autogator/configurations.py
@@ -2,3 +2,11 @@
 #
 # The entire module looks for serial numbers in this file as the single source of truth.
 
+#
+#
+# SERIAL NUMBERS
+#
+#
+stage_x = 27003356
+stage_y = 27003323
+piezo_motor = 71874833
