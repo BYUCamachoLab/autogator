67f02d374f4a6ba3a607e0facedfcec7a1a82db1
sequoiap <sequoiap4@gmail.com>
2019-08-28 20:03:20 -0600

    Windows can center themselves

--- autogator/ui/__init__.py
+++ autogator/ui/__init__.py
@@ -6,3 +6,11 @@ class Window(object):
         self.master = master
         self.master.title(title if title is not None else "tk")
         self.master.iconbitmap(os.path.join(os.path.dirname(autogator.__file__), 'images', icon))
+
+    def center(self):
+        self.master.update_idletasks()
+        width = self.master.winfo_width()
+        height = self.master.winfo_height()
+        x = (self.master.winfo_screenwidth() // 2) - (width // 2)
+        y = (self.master.winfo_screenheight() // 2) - (height // 2)
+        self.master.geometry('{}x{}+{}+{}'.format(width, height, x, y))
