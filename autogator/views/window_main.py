d9c989b185d00225ff1fc0b9b306dd3292e07456
Sequoia Ploeg <sequoiap4@gmail.com>
2020-07-02 17:38:21 -0600

    Created tool to compile from .ui to .py

---
+++ autogator/views/window_main.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © Autogator Project Contributors
+# Licensed under the terms of the MIT License
+# (see autogator/__init__.py for details)
+
+import sys
+
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtCore import QFile, QIODevice, Slot
+from PySide2.QtWidgets import QMainWindow, QApplication
+
+from autogator.compiled.window_main_ui import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):  # , model, main_controller):
+        super().__init__()
+
+        # self._model = model
+        # self._main_controller = main_controller
+        self._ui = Ui_MainWindow()
+        self._ui.setupUi(self)
+
+        # connect widgets to controller
+        # self._ui.spinBox_amount.valueChanged.connect(self._main_controller.change_amount)
+        # self._ui.pushButton_reset.clicked.connect(lambda: self._main_controller.change_amount(0))
+
+        # listen for model event signals
+        # self._model.amount_changed.connect(self.on_amount_changed)
+        # self._model.even_odd_changed.connect(self.on_even_odd_changed)
+        # self._model.enable_reset_changed.connect(self.on_enable_reset_changed)
+
+        # set a default value
+        # self._main_controller.change_amount(42)
+
+    # @pyqtSlot(int)
+    # def on_amount_changed(self, value):
+    #     self._ui.spinBox_amount.setValue(value)
+
+    # @pyqtSlot(str)
+    # def on_even_odd_changed(self, value):
+    #     self._ui.label_even_odd.setText(value)
+
+    # @pyqtSlot(bool)
+    # def on_enable_reset_changed(self, value):
+    #     self._ui.pushButton_reset.setEnabled(value)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
---
+++ tools/ui2py.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © Autogator Project Contributors
+# Licensed under the terms of the MIT License
+# (see autogator/__init__.py for details)
+
+"""
+Tool that converts all .ui and .qrc files in the autogator.resources folder to
+python files in autogator.compiled.
+
+Usage:
+$ python3 ui2py.py
+"""
+
+import sys
+import os
+import subprocess
+
+os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
+
+path = 'autogator'
+res = os.path.join(path, 'resources')
+dest = os.path.join(path, 'compiled')
+
+for root, directories, filenames in os.walk(res):
+    for filename in filenames:
+        item = os.path.join(root, filename)
+        if item.endswith('.ui'):
+            name, _ = os.path.splitext(filename)
+            rename = name + '_ui' + '.py'
+            path2dest = os.path.join(dest, rename)
+            print(*['pyside2-uic', '--from-imports', item, '-o', path2dest])
+            subprocess.call(['pyside2-uic', '--from-imports', item, '-o', path2dest])
+        if item.endswith('.qrc'):
+            name, _ = os.path.splitext(filename)
+            rename = name + '_rc' + '.py'
+            path2dest = os.path.join(dest, rename)
+            args = ['pyside2-rcc', item, '-o', path2dest]
+            print(*args)
+            subprocess.call(args)
