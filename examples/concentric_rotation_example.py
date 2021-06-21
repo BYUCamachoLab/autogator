20a94e8ce673b76f72c8dda277284db30823fcdc
unknown <camacho@CB400-EE10840.et.byu.edu>
2021-06-21 13:31:39 -0600

    Concentric Rotation

--- examples/concentric_rotation_example.py
+++ examples/concentric_rotation_example.py
@@ -3,6 +3,8 @@ import autogator.dataCache
 cache = autogator.dataCache.DataCache.get_instance()
 cache.load_configuration()
 mot = cache.motion.get_instance()
+original_step_size = mot.r_mot.jog_step_size
+mot.r_mot.jog_step_size = 10
 #for i in range(1):
 input("Press Enter For Next Rotation: ")
 mot.concentric_rotatation()
@@ -15,3 +17,4 @@ input("Press Enter For Next Rotation: ")
 mot.concentric_rotatation("backward")
 input("Press Enter For Next Rotation: ")
 mot.concentric_rotatation("backward")
+mot.r_mot.jog_step_size = original_step_size
