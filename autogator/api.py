ac1dcdc05ff301fa4ffc60993d0c50c02c465a69
sequoiap <sequoiap4@gmail.com>
2022-02-11 00:15:23 -0700

    No functions defined in api

--- autogator/api.py
+++ autogator/api.py
@@ -10,80 +10,21 @@
 A series of convenience functions.
 """
 
-import pathlib
-import shutil
-from typing import Union
-
-import numpy as np
-
 from autogator import AUTOGATOR_DATA_DIR, AUTOGATOR_CONFIG_DIR
-from autogator.experiment import Experiment
+from autogator.experiment import Experiment, ExperimentRunner
 from autogator.hardware import (
     HardwareConfiguration, 
     StageConfiguration, 
-    _CALIBRATION_PARAM_FILE, 
-    _DEFAULT_STAGE_CONFIGURATION,
+    load_default_configuration,
+    save_default_configuration,
 )
 
 
-def set_calibration_matrix(filename: Union[str, pathlib.Path]):
-    """
-    Set the calibration matrix for the stage.
-
-    Parameters
-    ----------
-    filename : str or pathlib.Path
-        The filename of the calibration matrix to use.
-    """
-    if isinstance(filename, str):
-        filename = pathlib.Path(filename)
-    if not filename.is_file():
-        raise ValueError("Calibration matrix file not found.")
-    shutil.copyfile(filename, _CALIBRATION_PARAM_FILE)
-
-
-def get_calibration_matrix() -> np.ndarray:
-    """
-    Get the calibration matrix for the stage.
-
-    Returns
-    -------
-    np.ndarray
-        The most recent calibration matrix, if it exists, else None.
-    """
-    return np.loadtxt(_CALIBRATION_PARAM_FILE) if _CALIBRATION_PARAM_FILE.is_file() else None
-
-
-def invalidate_calibration_matrix() -> None:
-    """
-    Invalidates the default calibration matrix for the stage.
-    """
-    if _CALIBRATION_PARAM_FILE.is_file():
-        _CALIBRATION_PARAM_FILE.unlink()
-    
-
-def load_default_configuration() -> StageConfiguration:
-    """
-    Loads the default stage configuration.
-
-    Returns
-    -------
-    StageConfiguration
-        The default stage configuration.
-    """
-    if not _DEFAULT_STAGE_CONFIGURATION.is_file():
-        raise ValueError("Default stage configuration not configured.")
-    return StageConfiguration.parse_file(_DEFAULT_STAGE_CONFIGURATION)
-
-
-def save_default_configuration(config: StageConfiguration) -> None:
-    """
-    Saves the default stage configuration.
-
-    Parameters
-    ----------
-    config : StageConfiguration
-        The stage configuration to save.
-    """
-    with _DEFAULT_STAGE_CONFIGURATION.open("w") as f:
-        f.write(config.json())
+__all__ = [
+    "HardwareConfiguration",
+    "StageConfiguration",
+    "load_default_configuration",
+    "save_default_configuration",
+    "Experiment",
+    "ExperimentRunner",
+]    
