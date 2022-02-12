1684d8b8f24f72b444f99590a26cfce1dec49202
sequoiap <sequoiap4@gmail.com>
2022-02-12 02:22:14 -0700

    Clean up calibration script

--- examples/calibration.py
+++ examples/calibration.py
@@ -1,16 +1,41 @@
-from json import load
+# -*- coding: utf-8 -*-
+#
+# Copyright © Autogator Project Contributors
+# Licensed under the terms of the GPLv3+ License
+# (see autogator/__init__.py for details)
 
-import numpy as np
+"""
+# Calibration
+
+Calibrates the stage so that circuits can be accessed by GDS coordinate
+instead of motor coordinates.
+
+Ordinarily, you would save the calibration matrix to a file, or associate
+it with a setup. This example simply prints the calibration matrix to the
+screen.
+"""
 
 from autogator.api import load_default_configuration
 from autogator.circuit import CircuitMap
-from autogator.routines import KeyboardControl, KeyloopKeyboardBindings, auto_scan, basic_scan, calibrate, easy_switcher
+from autogator.controllers import KeyboardControl, KeyloopKeyboardBindings
+from autogator.routines import auto_scan, calibrate
 
 
 def scope_configure_single_meas():
+    """
+    Configure the Rohde & Schwarz oscilloscope for single-shot measurements.
+
+    We use the API defined in PyroLab
+    (https://pyrolab.readthedocs.io/en/latest/) which is wrapped in 
+    ``autogator.hardware``. Because it implements
+    ``autogator.hardware.DataAcquisitionUnitBase``, it provides a ``measure()``
+    method that can is used to detect intensity at each measurement point.
+
+    In this example, we use channel 1 for our signal.
+    """
     oscope = stage.scope.driver
 
-    # Scope setup
+    # Scope setup parameters
     CHANNEL = 1
     RANGE = 0.6
     COUPLING = "DCLimit"
@@ -22,28 +47,38 @@ def scope_configure_single_meas():
 
 
 def laser_configure():
+    """
+    Configure the laser for what we believe will be the maximum signal, 1550 nm.
+
+    In this example, we are using the Santec TSL-550 laser as implemented by
+    PyroLab.
+    """
     laser = stage.laser
     laser.on()
     laser.power(-4.0)
     laser.wavelength(1550.0)
 
 
-def keyboard_control():
+def keyboard_control(stage):
+    """
+    Default keyboard control loop for manual motion.
+    """
     kc = KeyboardControl(stage, KeyloopKeyboardBindings())
     kc.loop()
-
-
-def next_loc():
-    yield from [
-        [9.9237, 11.5935, None, None, None, 100.0],
-        [10.0975, 3.3209, None, None, None, 100.0],
-        [1.1629, 3.1288, None, None, None, 100.0],
-    ]
 
 
 def auto_calibration_callback(stage, daq, circuit, controller) -> None:
     """
     Moves to the specified circuit and optimizes alignment.
+
+    ``calibrate()`` requires a callback function that takes the stage,
+    DAQ, circuit, and controller as arguments.
+
+    We use this callback to move to the next location and optimize the
+    alignment. We have roughly found where the light is at each location,
+    so this function automatically moves to the next location and 
+    optimizes the alignment. If it fails, it then allows you to manually
+    move the stage before it attempts to optimize again.
 
     Parameters
     ----------
@@ -56,7 +91,6 @@ def auto_calibration_callback(stage, daq, circuit, controller) -> None:
     controller : KeyboardControl
         The controller to use for control input.
     """
-
     SCAN_SPAN = 0.035
     SCAN_STEP = 0.0024
 
@@ -78,42 +112,42 @@ def auto_calibration_callback(stage, daq, circuit, controller) -> None:
     print("Exiting callback")
 
 
+def next_loc():
+    """
+    Generator function. Yields the next location in the list of locations.
+
+    This function is used by ``auto_calibration_callback()`` to move to the
+    next location in the list of locations. These locations are roughly
+    the stage positions where we'll find light (this will be different
+    almost every time you swap out a chip or change setups, so update these
+    values accordingly).
+
+    These locations should correspond, in the same order, to the calibration
+    functions as found in the CircuitMap.
+
+    Returns
+    -------
+    next_pos : float
+        The next location in the list of locations.
+    """
+    yield from [
+        [9.9237, 11.5935, None, None, None, 100.0],
+        [10.0975, 3.3209, None, None, None, 100.0],
+        [1.1629, 3.1288, None, None, None, 100.0],
+    ]
+
+
 if __name__ == "__main__":
     cmap = CircuitMap.loadtxt("data/circuitmap.txt")
     calib = cmap.filterby(calibration_circuit="True")
 
-    scfg = load_default_configuration()
-    stage = scfg.get_stage()
+    stage = load_default_configuration().get_stage()
 
     scope_configure_single_meas()
     laser_configure()
 
-    SCAN_SPAN = 0.06
-    SCAN_STEP = 0.0024
+    locs = next_loc()
 
-    def do_auto_scan():
-        auto_scan(stage=stage, daq=stage.scope, span=SCAN_SPAN, step_size=SCAN_STEP, go_to_max=True)
-
-    def do_basic_scan():
-        return basic_scan(stage=stage, daq=stage.scope, span=SCAN_SPAN, step_size=SCAN_STEP, plot=True)
-
-    def do_auto_scan():
-        return auto_scan(stage=stage, daq=stage.scope, settle=0.0, plot=True)
-
-    def do_calibration():
-        return calibrate(stage=stage, daq=stage.scope, circuits=calib, callback=auto_calibration_callback)
-
-    def do_easy_switcher():
-        return easy_switcher(stage=stage, daq=stage.scope)
-
-
-    # stage.load_calibration_matrix("calib_mat.txt")
-
-    # pos = {}
-    # pos = {1: [1.1629, 3.1288, None, None, None, 100.0], 2: [10.0975, 3.3209, None, None, None, 100.0], 3: [9.9237, 11.5935, None, None, None, 100.0]}
-    # locs = next_loc()
-    # matrix = do_calibration()
-    
-    # cmat = do_calibration()
-    # print(cmat)
-    # calibrate(stage, stage.scope, calib, auto_calibration_callback)
+    matrix = calibrate(stage=stage, daq=stage.scope, circuits=calib, callback=auto_calibration_callback)
+    print("Calibration matrix:")
+    print(matrix)
